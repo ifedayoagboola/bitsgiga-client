59af85ab36d7a2f7d48da5c68ba30b5365b9f6f1
mexuvo <rafiqulislamsuvobd@gmail.com>
2022-06-20 18:45:09 +0600

    add component but not complete yet

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,5 +1,6 @@
 import datas from "../../data/products.json";
 import SectionStyleOne from "../Helpers/SectionStyleOne";
+import ViewMoreTitle from "../Helpers/ViewMoreTitle";
 import Layout from "../Partials/Layout";
 import Banner from "./Banner";
 import BrandSection from "./BrandSection";
@@ -27,7 +28,19 @@ export default function Home() {
           sectionTitle="Shop by Brand"
           className="brand-section-wrapper mb-[60px]"
         />
-        <CampaignCountDown lastDate="2023-03-04 4:00:00" />
+        <CampaignCountDown
+          className="mb-[60px]"
+          lastDate="2023-03-04 4:00:00"
+        />
+        <ViewMoreTitle
+          className="top-selling-product mb-[60px]"
+          seeMoreUrl="#"
+          categoryTitle="Top Selling Products"
+        >
+          <div>
+            <p>suvo</p>
+          </div>
+        </ViewMoreTitle>
       </Layout>
     </>
   );
