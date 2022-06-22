10f490e3331f7e20ddee15204063b1fdc5a4d319
mexuvo <rafiqulislamsuvobd@gmail.com>
2022-06-22 13:49:53 +0600

    cart widget added

--- src/components/Partials/Header/Middlebar.jsx
+++ src/components/Partials/Header/Middlebar.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import Cart from "../../Cart";
 import Compair from "../../Helpers/icons/Compair";
 import ThinBag from "../../Helpers/icons/ThinBag";
 import ThinLove from "../../Helpers/icons/ThinLove";
@@ -5,60 +7,75 @@ import ThinPeople from "../../Helpers/icons/ThinPeople";
 import SearchBox from "../../Helpers/SearchBox";
 
 export default function Middlebar({ className }) {
+  const [toggleCart, setToggle] = useState(false);
+  const cartHandler = () => {
+    setToggle(!toggleCart);
+  };
   return (
     <div className={`w-full h-[86px] bg-white ${className}`}>
       <div className="container-x mx-auto h-full">
-        <div className="flex justify-between items-center h-full">
-          <div>
-            <img
-              width="152"
-              height="36"
-              src={`${process.env.PUBLIC_URL}/assets/images/logo.svg`}
-              alt="logo"
-            />
-          </div>
-          <div className="w-[517px] h-[44px]">
-            <SearchBox className="search-com" />
-          </div>
-          <div className="flex space-x-6 items-center">
-            <div className="compaire relative">
-              <button type="button">
-                <span>
-                  <Compair />
-                </span>
-              </button>
-              <span className="w-[18px] h-[18px] rounded-full bg-qyellow absolute -top-2.5 -right-2.5 flex justify-center items-center text-[9px]">
-                2
-              </span>
-            </div>
-            <div className="favorite relative">
-              <button type="button">
-                <span>
-                  <ThinLove />
-                </span>
-              </button>
-              <span className="w-[18px] h-[18px] rounded-full bg-qyellow absolute -top-2.5 -right-2.5 flex justify-center items-center text-[9px]">
-                1
-              </span>
-            </div>
-            <div className="cart relative">
-              <button type="button">
-                <span>
-                  <ThinBag />
-                </span>
-              </button>
-              <span className="w-[18px] h-[18px] rounded-full bg-qyellow absolute -top-2.5 -right-2.5 flex justify-center items-center text-[9px]">
-                15
-              </span>
-            </div>
+        <div className="relative h-full">
+          <div className="flex justify-between items-center h-full">
             <div>
-              <button type="button">
-                <span>
-                  <ThinPeople />
+              <img
+                width="152"
+                height="36"
+                src={`${process.env.PUBLIC_URL}/assets/images/logo.svg`}
+                alt="logo"
+              />
+            </div>
+            <div className="w-[517px] h-[44px]">
+              <SearchBox className="search-com" />
+            </div>
+            <div className="flex space-x-6 items-center">
+              <div className="compaire relative">
+                <button type="button">
+                  <span>
+                    <Compair />
+                  </span>
+                </button>
+                <span className="w-[18px] h-[18px] rounded-full bg-qyellow absolute -top-2.5 -right-2.5 flex justify-center items-center text-[9px]">
+                  2
                 </span>
-              </button>
+              </div>
+              <div className="favorite relative">
+                <button type="button">
+                  <span>
+                    <ThinLove />
+                  </span>
+                </button>
+                <span className="w-[18px] h-[18px] rounded-full bg-qyellow absolute -top-2.5 -right-2.5 flex justify-center items-center text-[9px]">
+                  1
+                </span>
+              </div>
+              <div className="cart relative cursor-pointer">
+                <button onClick={cartHandler} type="button">
+                  <span>
+                    <ThinBag />
+                  </span>
+                </button>
+                <span className="w-[18px] h-[18px] rounded-full bg-qyellow absolute -top-2.5 -right-2.5 flex justify-center items-center text-[9px]">
+                  15
+                </span>
+              </div>
+              <div>
+                <button type="button">
+                  <span>
+                    <ThinPeople />
+                  </span>
+                </button>
+              </div>
             </div>
           </div>
+          {toggleCart && (
+            <>
+              <div
+                onClick={cartHandler}
+                className="fixed left-0 top-0 w-full h-full z-40"
+              ></div>
+              <Cart className="absolute right-0 top-16 z-50" />
+            </>
+          )}
         </div>
       </div>
     </div>
