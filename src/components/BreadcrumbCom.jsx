fdb7e496cc4a22ecc5a41c9042760da4481d3f62
mexuvo <rafiqulislamsuvobd@gmail.com>
2022-06-25 18:24:24 +0600

    breadcom component added

---
+++ src/components/BreadcrumbCom.jsx
@@ -0,0 +1,15 @@
+export default function BreadcrumbCom() {
+  return (
+    <>
+      <div className="font-400 text-[13px] text-qblack mb-[23px]">
+        <a href="#">
+          <span className="mx-1">Home Page</span>
+        </a>
+        /
+        <a href="#">
+          <span className="mx-1">Shop</span>
+        </a>
+      </div>
+    </>
+  );
+}
