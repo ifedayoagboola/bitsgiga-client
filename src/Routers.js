2736df6e26b460e91f3fe1e9e3787b506f38c309
mexuvo <rafiqulislamsuvobd@gmail.com>
2022-06-25 18:21:55 +0600

    new page added

--- src/Routers.js
+++ src/Routers.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
+import AllProductPage from "./components/AllProductWidget";
 import Home from "./components/Home";
 
 export default function Routers() {
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
+      <Route exact path="/all-products" element={<AllProductPage />} />
     </Routes>
   );
 }
