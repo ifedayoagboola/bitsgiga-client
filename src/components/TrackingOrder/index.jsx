3e0becf35ef0ff144bafa94a7862d1368fcbaf99
mexuvo <rafiqulislamsuvobd@gmail.com>
2022-07-02 13:46:38 +0600

    tracking page done

--- src/components/TrackingOrder/index.jsx
+++ src/components/TrackingOrder/index.jsx
@@ -1,9 +1,59 @@
+import InputCom from "../Helpers/InputCom";
+import PageTitle from "../Helpers/PageTitle";
 import Layout from "../Partials/Layout";
+import Thumbnail from "./Thumbnail";
 
 export default function TrackingOrder() {
   return (
     <Layout childrenClasses="pt-0 pb-0">
-      <div>suvo</div>
+      <div className="tracking-page-wrapper w-full">
+        <div className="page-title mb-[40px]">
+          <PageTitle
+            title="Track Order"
+            breadcrumb={[
+              { name: "home", path: "/" },
+              { name: "Track Order", path: "/tracking-order" },
+            ]}
+          />
+        </div>
+        <div className="content-wrapper w-full mb-[40px]">
+          <div className="container-x mx-auto">
+            <h1 className="text-[22px] text-qblack font-semibold leading-9">
+              Track Your Order
+            </h1>
+            <p className="text-[15px] text-qgraytwo leading-8 mb-5">
+              Enter your order taracking number and your secreet id.
+            </p>
+            <div className="w-full bg-white px-[30px] py-[23px] flex items-center">
+              <div className="w-[642px]">
+                <div className="mb-3">
+                  <InputCom
+                    placeholder="Order Number"
+                    label="Order Tracking Number*"
+                    inputClasses="w-full h-[50px]"
+                  />
+                </div>
+                <div className="mb-[30px]">
+                  <InputCom
+                    placeholder="23/09/2022"
+                    label="Delivery Date"
+                    inputClasses="w-full h-[50px]"
+                  />
+                </div>
+
+                <a href="#">
+                  <div className="w-[142px] h-[50px] black-btn flex justify-center items-center">
+                    <span>Track Now</span>
+                  </div>
+                </a>
+              </div>
+              <div className="flex-1 flex justify-center">
+                <Thumbnail />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
