5e6270dfd1c5e737a4c96f63f2bfe237598a6e34
mexuvo <rafiqulislamsuvobd@gmail.com>
2022-07-07 13:08:46 +0600

    blog page responsive done

--- src/components/Blogs/index.jsx
+++ src/components/Blogs/index.jsx
@@ -22,7 +22,7 @@ export default function Blogs() {
       <div className="w-full py-[60px]">
         <div className="container-x mx-auto">
           <div className="w-full">
-            <div className="grid grid-cols-2 gap-[30px]">
+            <div className="grid md:grid-cols-2 grid-cols-1 lg:gap-[30px] gap-5">
               <DataIteration
                 datas={blog.blogs}
                 startLength={0}
