305a31224a614d54468a8df79d2011b5f64527e6
mexuvo <rafiqulislamsuvobd@gmail.com>
2022-07-27 16:57:42 +0600

    some section done

--- src/components/HomeTwo/index.jsx
+++ src/components/HomeTwo/index.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import LayoutHomeTwo from "../Partials/LayoutHomeTwo";
 
 import datas from "../../data/productsTwo.json";
+import SectionStyleFour from "../Helpers/SectionStyleFour";
 import SectionStyleThreeHomeTwo from "../Helpers/SectionStyleThreeHomeTwo";
+import ProductsAds from "../Home/ProductsAds";
 import Banner from "./Banner";
 
 export default function HomeTwo() {
@@ -17,6 +19,14 @@ export default function HomeTwo() {
         seeMoreUrl="/all-products"
         className="new-products mb-[60px]"
       />
+      <ProductsAds
+        ads={[
+          `${process.env.PUBLIC_URL}/assets/images/ads-2.2.png`,
+          `${process.env.PUBLIC_URL}/assets/images/ads-2.1.png`,
+        ]}
+        sectionHeight="sm:h-[290px] h-full"
+        className="products-ads-section mb-[60px]"
+      />
       <SectionStyleThreeHomeTwo
         products={products.slice(3, 7)}
         showProducts={3}
@@ -24,12 +34,27 @@ export default function HomeTwo() {
         seeMoreUrl="/all-products"
         className="feature-products mb-[60px]"
       />
+      <ProductsAds
+        ads={[`${process.env.PUBLIC_URL}/assets/images/ads-2.3.png`]}
+        className="products-ads-section mb-[60px]"
+      />
       <SectionStyleThreeHomeTwo
         products={products.reverse().slice(0, 10)}
         showProducts={9}
         sectionTitle="New Arrivals"
         seeMoreUrl="/all-products"
         className="new-arrivals mb-[60px]"
+      />
+      <ProductsAds
+        sectionHeight="164"
+        ads={[`${process.env.PUBLIC_URL}/assets/images/ads-2.4.png`]}
+        className="products-ads-section mb-[60px]"
+      />
+      <SectionStyleFour
+        products={products}
+        sectionTitle="Popular Sales"
+        seeMoreUrl="/all-products"
+        className="category-products mb-[60px]"
       />
     </LayoutHomeTwo>
   );
