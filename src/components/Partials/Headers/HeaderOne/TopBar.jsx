af724137c133f17dbc29164ad85e75d934c05920
ifedayo (Sam) <ifedayoagboola19@gmail.com>
2025-06-08 22:28:50 +0100

    update topbar

--- src/components/Partials/Headers/HeaderOne/TopBar.jsx
+++ src/components/Partials/Headers/HeaderOne/TopBar.jsx
@@ -40,33 +40,15 @@ export default function TopBar({ className }) {
             <div className="topbar-dropdowns sm:block hidden">
               <div className="flex space-x-6">
                 <div className="country-select flex space-x-1 items-center">
-                  <div>
-                    <img
-                      src={`${
-                        import.meta.env.VITE_PUBLIC_URL
-                      }/assets/images/country-logo-16x16.png`}
-                      width="16"
-                      height="16"
-                      alt="country logo"
-                      className="overflow-hidden rounded-full"
-                    />
-                  </div>
                   <Selectbox
                     className="w-fit"
-                    datas={["United State", "Bangladesh", "India"]}
+                    datas={["United Kingdom", "Nigeria"]}
                   />
                   <div>
                     <Arrow className="fill-current qblack" />
                   </div>
                 </div>
-                <div className="currency-select flex space-x-1 items-center">
-                  <Selectbox className="w-fit" datas={["USD", "BDT"]} />
-                  <Arrow className="fill-current qblack" />
-                </div>
-                <div className="language-select flex space-x-1 items-center">
-                  <Selectbox className="w-fit" datas={["Bangla", "english"]} />
-                  <Arrow className="fill-current qblack" />
-                </div>
+             
               </div>
             </div>
           </div>
