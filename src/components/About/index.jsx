bf6da1a3e8ed7a0d1795852078dff871fc86143b
ifedayo (Sam) <ifedayoagboola19@gmail.com>
2025-08-29 18:41:15 +0100

    testing dev

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -78,7 +78,7 @@ export default function About() {
                   What is e-commerce business?
                 </h1>
                 <p className="text-[15px] text-qgraytwo leading-7 mb-2.5">
-                  Testing dev.
+                  Testing dev2.
                   Lorem Ipsum is simply dummy text of the printing and
                   typesetting industry. Lorem Ipsum has been the industry's
                   standard dummy text ever since the 1500s, when an unknown
